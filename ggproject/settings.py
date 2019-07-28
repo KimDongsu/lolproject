b8a8ad2eebe405beb3c89257620785b828fd1bef
unknown <kdsdsk4425@naver.com>
2019-07-29 08:40:42 +0900

    lol

--- ggproject/settings.py
+++ ggproject/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zz5j%1mo_zldq()4+ye(^#3=c$k(dwn!1(!u*(u3(zrva^#+9b'
+#SECRET_KEY = 'zz5j%1mo_zldq()4+ye(^#3=c$k(dwn!1(!u*(u3(zrva^#+9b'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
